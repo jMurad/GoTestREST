4df46109169c88a497a3f659016944af4cda85c9
Murad <deit91@ya.ru>
2020-08-05 11:58:44 +0300

    day 2

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,18 +1,38 @@
 package apiserver
 
+import "github.com/sirupsen/logrus"
+
 // APIServer ...
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
 }
 
 // New ...
 func New(config *Config) *APIServer {
-	return &APIServer {
+	return &APIServer{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 // Start ...
 func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	return nil
+
+	s.logger.Info("starting api server")
+}
+
+// ff
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+	s.logger.SetLevel(level)
 	return nil
 }
